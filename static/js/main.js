ca18334acbbf7adbfb528bb64d1a4f1d1acf07b0
nerincon <nerincon1@gmail.com>
2018-07-19 00:38:30 -0500

    add projects, add css, remove contact for now, remove hobbies for now

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $('#blackjack').click(function () {
+    window.open('https://nerincon.github.io/blackjack_game/', '_blank')
+  })
+  $('#campground').click(function () {
+    window.open('https://fathomless-sands-66891.herokuapp.com/', '_blank')
+  })
+  $('#wav3').click(function () {
+    window.open('https://wav3space.herokuapp.com/', '_blank')
+  })
+  $('#weather').click(function () {
+    window.open('https://get-city-weather.herokuapp.com', '_blank')
+  })
+  $('#crm').click(function () {
+    window.open('https://kappacrm.herokuapp.com/', '_blank')
+  })
+  $('#github').click(function () {
+    window.open('https://github.com/nerincon', '_blank')
+  })
+  $('#linkedin').click(function () {
+    window.open('https://www.linkedin.com/in/nelson-rincon-52314725/', '_blank')
+  })
+})
